705f335e562af1e80643ad0f137f461e32ec518c
Lyudmil Goranov <102062855+Goran0v@users.noreply.github.com>
2025-03-17 18:44:04 +0200

    Add the exercise

---
+++ Workshop/Eventmi.Infrastructure/Data/Models/Event.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace Eventmi.Infrastructure.Data.Models
+{
+    [Comment("Събития")]
+    public class Event
+    {
+        [Key]
+        [Comment("Идентификатор на запис")]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        [Comment("Име на събитието")]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [Comment("Начална дата и час")]
+        public DateTime StartDate { get; set; }
+
+        [Required]
+        [Comment("Крайна дата и час")]
+        public DateTime EndDate { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        [Comment("Място на събитието")]
+        public string Place { get; set; } = null!;
+    }
+}
