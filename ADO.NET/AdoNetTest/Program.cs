a9d4dffa190ec3071cce0e8c1e74ae76276d5462
Lyudmil Goranov <102062855+Goran0v@users.noreply.github.com>
2025-03-17 18:14:37 +0200

    Add the exercise

---
+++ ADO.NET/AdoNetTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Data.SqlClient;
+
+SqlConnection connection = new SqlConnection(@"Server=.;Database=SoftUni;Integrated Security=true; Trust Server Certificate=true");
+
+connection.Open();
+
+using (connection)
+{
+    SqlCommand command = new SqlCommand("SELECT * FROM Employees WHERE DepartmentId = 7", connection);
+    SqlDataReader reader = command.ExecuteReader();
+
+    using (reader)
+    {
+        while (reader.Read())
+        {
+            string? firstName = reader["FirstName"].ToString();
+            string? lastName = reader["LastName"].ToString();
+
+            Console.WriteLine($"{firstName} {lastName}");
+        }
+    }
+}
