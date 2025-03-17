e28ab57df1bb78322cfce2b7e06144ce09a9b4f7
Lyudmil Goranov <102062855+Goran0v@users.noreply.github.com>
2025-03-17 18:28:55 +0200

    Add the second part of the exercise

---
+++ Auto Mapping Objects/FastFood.Common/DataConfiguration/Config.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastFood.Common.DataConfiguration
+{
+    public static class Config
+    {
+        public const string ConnectionString = @"Server=DESKTOP-M3PT2UP;Database=FastFood2023;Integrated Security=True;Encrypt=False";
+    }
+}
---
+++ Auto Mapping Objects/FastFood.Services.Class.Mapping/FastFoodProfile.cs
@@ -0,0 +1,18 @@
+﻿namespace FastFood.Services.Class.Mapping
+{
+    using AutoMapper;
+    using FastFood.Models;;
+
+    public class FastFoodProfile : Profile
+    {
+        public FastFoodProfile()
+        {
+            //Positions
+            CreateMap<CreatePositionInputModel, Position>()
+                .ForMember(x => x.Name, y => y.MapFrom(s => s.PositionName));
+
+            CreateMap<Position, PositionsAllViewModel>()
+                .ForMember(x => x.Name, y => y.MapFrom(s => s.Name));
+        }
+    }
+}
