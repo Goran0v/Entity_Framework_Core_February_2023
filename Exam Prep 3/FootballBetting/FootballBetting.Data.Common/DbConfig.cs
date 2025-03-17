94997bb978b84651d79dabf6731807c4cfb472b2
Lyudmil Goranov <102062855+Goran0v@users.noreply.github.com>
2025-03-17 18:50:59 +0200

    Add the exercise

---
+++ Exam Prep 3/FootballBetting/FootballBetting.Data.Common/DbConfig.cs
@@ -0,0 +1,7 @@
+﻿namespace FootballBetting.Data.Common
+{
+    public static class DbConfig
+    {
+        public const string ConnectionString = @"Server=DESKTOP-M3PT2UP;Database=Bet388; Integrated Security=True;Encrypt=False";
+    }
+}
---
+++ Exam Prep 3/FootballBetting/FootballBetting.Data.Common/ValidationConstants.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballBetting.Data.Common
+{
+    public static class ValidationConstants
+    {
+        public const int TeamNameMaxLength = 50;
+
+        public const int TeamLogoUrlMaxLength = 2048;
+
+        public const int TeamInitialsMaxLength = 4;
+
+        public const int ColorNameMaxLength = 10;
+
+        public const int TownNameMaxLength = 58;
+
+        public const int CountryNameMaxLength = 56;
+
+        public const int PlayerNameMaxLength = 100;
+
+        public const int PositionNameMaxLength = 50;
+
+        public const int GameResultMaxLength = 7;
+
+        public const int UsernameMaxLength = 36;
+
+        public const int UserPasswordMaxLength = 256;
+
+        public const int UserEmailMaxLength = 256;
+
+        public const int UserNameMaxLength = 100;
+    }
+}
